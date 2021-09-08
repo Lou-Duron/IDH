9ced1a78f6e834b3b813d2b1d570b83a14dcb0fb
lou <lou@localhost.localdomain>
2021-09-08 11:58:31 +0200

    script

--- projet_idh.py
+++ projet_idh.py
@@ -1,6 +1,43 @@
 from py2neo import Graph
-graph = Graph("bolt://localhost:7687", auth=("neo4j", "votremotdepassepourlabase"))
-q = "match (t:Track) return t.name as track"
-res = graph.run(q).to_table()
-for record in res :
-    print(record[0])
+
+graph = Graph("bolt://localhost:7687", auth=("neo4j", "a"))
+
+test = "{name:'Newark Chesterton'}"
+test2 = "{name:'Going Home'}"
+
+def fb(u,t):
+    listGenre = []
+    q = f"match (:Person{u})-[]->(:Track )-[]->(:Album)-[]->(:Artist)-[]->(g:Genre) return distinct g.name"
+    res = graph.run(q).to_table()
+    for el in res : 
+        listGenre.append(el[0])
+    q = f"match (:Track{t})-[]->(:Album)-[]->(:Artist)-[]->(g:Genre) return distinct g.name"
+    res = graph.run(q).to_table()
+    for el in res:
+        if el[0] in listGenre:
+            return 2
+    return 1
+    """
+    Si t est dans la liste des genres de u alors return 2
+    sinon return 1
+
+    !!!
+    Regler les  f string de merde
+    Meilleur retour de querry ? to_data ??
+    """
+
+def fs(u,t):
+    """
+    Nombre de PPC * la taille des PPC
+    """
+
+def ft(u,t):
+    """
+    Cohérence thématique (Jaccard mes couilles)
+    """
+
+def score(u, t,a=0.25, b=0.75):
+    return fb(u,t) * (a * fs(u,t) + b * ft(u,t))
+
+print(fb(test, test2))
+
