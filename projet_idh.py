b45638bdc23da79091d7bb328ccff946a068481d
lou <lou@localhost.localdomain>
2021-09-12 12:10:34 +0200

    parser

--- projet_idh.py
+++ projet_idh.py
@@ -1,10 +1,22 @@
-from py2neo import Graph
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+from py2neo import Graph
+import argparse
+
+################################################################################################
+parser = argparse.ArgumentParser(description='truc')
+parser.add_argument('--username', '-u', type=str, required=True, help="Name of the user")
+parser.add_argument('--track', '-t', type=str, required=True, help="Name of the track")
+parser.add_argument('--alpha', '-a', type=float, required=False, default=0.25, help="Alpha value (0.25 by default")
+parser.add_argument('--beta', '-b',  type=float, required=False, default=0.75, help="Beta value (0.75 by default")
+args = parser.parse_args()
+################################################################################################
+
+#Connection to Neo4J
 graph = Graph("bolt://localhost:7687", auth=("neo4j", "a"))
 
-test = "Newark Chesterton"
-test2 = "Going Home"
-
+# Adéquation au genre musical
 def fb(u,t):
     listGenre = []
     q = f"match (:Person{{name:'{u}'}})-[]->(:Track )-[]->(:Album)-[]->(:Artist)-[]->(g:Genre) return distinct g.name"
@@ -17,16 +29,13 @@ def fb(u,t):
         if el[0] in listGenre:
             return 2
     return 1
-    """
-    Si t est dans la liste des genres de u alors return 2
-    sinon return 1
 
-    !!!
-    Regler les  f string de merde
-    Meilleur retour de querry ? to_data ??
-    """
-
+# Facteur social
 def fs(u,t):
+    q = f"match path=allShortestPaths((u:Person{{name:'{u}'}})-[*]->(t:Track{{name:'{t}'}})) RETURN length(path), count(*)"
+    res = graph.run(q).to_table()
+    print(res)
+    return res[0][0] * res[0][1]
     """
     Nombre de PPC * la taille des PPC
     """
@@ -39,5 +48,7 @@ def ft(u,t):
 def score(u, t,a=0.25, b=0.75):
     return fb(u,t) * (a * fs(u,t) + b * ft(u,t))
 
-print(fb(test, test2))
+
+
+print(fs(test, test2))
 
