2f67be7b8d1a56946ce7d0e5cd3b4e66209d7422
Victoria FATHI <victoria.fathi@gmail.com>
2021-11-03 16:09:39 +0100

    add coverage

--- Part_2/Neo4J_Roland.py
+++ Part_2/Neo4J_Roland.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-# ./enrichment/blastsets.neo4j.py -q ecoli/query.sets/set.01.txt  -t Keyword -s 511145 -c
-
 import argparse
 from os.path import isfile
 from scipy.stats import binom, hypergeom
@@ -48,56 +46,56 @@ path = '[:is_a|part_of|annotates*]' if param.sets=='GOTerm' else ''
 cypher = f"MATCH (t:{param.sets})-{path}->(n:Gene {{taxon_id:{param.species} }}) WHERE n.id IN ['"+"', '".join(query)+"'] RETURN DISTINCT t"
 if param.verbose:
 	print(cypher)
-# nodes.match('Gene', taxon_id=511145, id=IN( ['b0001','b0002'] ) ).all()
-# links = RelationshipMatcher(neo)
 sets = neo.run(cypher)
-#table = neo.run(cypher).to_table()
-#set_ids = set([ table[i][0] for i in range(len(table)) ])
-#pprint(set_ids)
+
 
 # EVALUATE SETS
 i = 0
 results = []
 query_size = len(query)
+
 for s in sets: #_ids:
 	sid = s['t']['id']
 	# RETRIEVE TARGET SET ELEMENTS
-	#cypher = "MATCH (t:{})-[:is_a|part_of|annotates*]->(n:Gene {{taxon_id:{} }}) WHERE t.id='{}' RETURN n.id".format(param.sets, param.species, sid)
 	path = '[:is_a|part_of|annotates*1..20]' if param.sets=='GOTerm' else ''
 	cypher = "MATCH (t:{})-{}->(n:Gene {{taxon_id:{} }}) WHERE t.id='{}' RETURN n.id".format(param.sets, path, param.species, sid)
 	if param.verbose:
 		print(cypher)
 	table = neo.run(cypher).to_table()
-	#elements = set([ table[i][0] for i in range(len(table)) ])
-	#elements = set( list( zip(*table) )[0] )
 	elements = set( map(lambda x: x[0], table))
 	i += 1
 	print(i)
-	common_elements = elements.intersection( query )
+	common_elements = elements.intersection(query)
 	if len(common_elements) < 2:
 		next
-	if param.measure=='binomial': # binom.cdf(>=success, attempts, proba)
-		pvalue = binom.cdf( query_size - len(common_elements), query_size, 1 - float(len(elements))/population_size)
-	else:
-		print(f'sorry, {param.measure} not (yet) implemented')
-		exit(1)
-	r = { 'id': sid, 'desc': s['t']['desc'], 'common.n':len(common_elements), 'target.n': len(elements), 'p-value': pvalue, 'elements.target': elements, 'elements.common': common_elements }
+	if param.measure == 'coverage':
+		measure = (len(common_elements)/len(query)) * (len(common_elements)/len(elements))
+	if param.measure =='binomial': # binom.cdf(>=success, attempts, proba)
+		measure = binom.cdf( query_size - len(common_elements), query_size, 1 - float(len(elements))/population_size)
+	if param.measure =='hypergeometric':
+		break
+	if param.measure =='chi2':
+		break
+	#else:
+	#	print(f'sorry, {param.measure} not (yet) implemented')
+	#	exit(1)
+	r = { 'id': sid, 'desc': s['t']['desc'], 'common.n':len(common_elements), 'target.n': len(elements), 'measure': measure, 'elements.target': elements, 'elements.common': common_elements }
 	results.append( r )
 if param.verbose:
   print(results)
 
 # PRINT SIGNIFICANT RESULTS
-results.sort(key=lambda an_item: an_item['p-value'])
+results.sort(key=lambda an_item: an_item['measure'])
 i=1
 for r in results:
 	# FDR
-	if param.adjust and r['p-value'] > param.alpha * i / len(results): break
+	if param.adjust and r['measure'] > param.alpha * i / len(results): break
 	# limited output
 	if param.limit > 0 and i>param.limit: break
 	# alpha threshold
-	elif r['p-value'] > param.alpha: break
+	elif r['measure'] > param.alpha: break
 	# OUTPUT
-	pval = "{:.4f}".format(r['p-value']) if r['p-value']>0.01 else "{:.2e}".format(r['p-value'])
+	pval = "{:.4f}".format(r['measure']) if r['measure']>0.01 else "{:.2e}".format(r['measure'])
 	print("{}\t{}\t{}/{}\t{}\t{}".format( r['id'], pval, r['common.n'], r['target.n'], r['desc'], ', '.join(r['elements.common'])))
 	i+=1
 
