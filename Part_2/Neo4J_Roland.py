725dc0dd29f23603d10ed9b7a87a63dd45787762
loudu <lou.duron@gmail.fr>
2021-11-09 11:57:27 +0100

    metrics evaluation update

--- Part_2/Neo4J_Roland.py
+++ Part_2/Neo4J_Roland.py
@@ -6,20 +6,35 @@ from scipy.stats import binom, hypergeom, chi2_contingency
 from py2neo import *
 from py2neo.matching import *
 import numpy as np
+import pprint
+import time
 
-# SCRIPT PARAMETERS
+start_time = time.time() # Execution time
+
+# Parameters
 parser = argparse.ArgumentParser(description='Search enriched terms/categories in the provided (gene) set')
 parser.add_argument('-q', '--query', required=True, help='Query set.')
-parser.add_argument('-t', '--sets', required=True, help='Target sets node type.')
+parser.add_argument('-t', '--type', required=True, help='Target sets node type.')
 parser.add_argument('-s', '--species', required=True, type=int, help='Taxon id.')
+parser.add_argument('-p', '--password', required=True, help='Password to connect to the DBMS')
 parser.add_argument('-a', '--alpha', required=False, type=float, default=0.05, help='Significance threshold.')
 parser.add_argument('-c', '--adjust', required=False, action="store_true", help='Adjust for multiple testing (FDR).')
-parser.add_argument('-m', '--measure', required=False, default='binomial', help='Dissimilarity index: binomial (default), hypergeometric, chi2, coverage or all.')
+parser.add_argument('-m', '--measure', required=False, default='binomial', help='Dissimilarity index: binomial (default), hypergeometric, chi2, coverage .')
 parser.add_argument('-l', '--limit', required=False, type=int, default=0, help='Maximum number of results to report.')
+parser.add_argument('-e', '--eval', required=False, action="store_true", help='Metrics evaluation if true')
 parser.add_argument('-v', '--verbose', required=False, action="store_true", help='Talk a lot.')
 arg = parser.parse_args()
 
-# LOAD QUERY
+# DBMS connection
+try:
+	neo = Graph("bolt://localhost:7687", auth=("neo4j", arg.password))
+	nodes = NodeMatcher(neo)
+	print('Connection to the DBMS successful')
+except Exception:
+	print('An error occurred while trying to connect to the DBMS please check your password')
+	exit(1)
+
+# Query loading
 text = arg.query
 query = set()
 if isfile(text):
@@ -28,83 +43,118 @@ if isfile(text):
 		query |= set(content)
 else: # parse string
 	query |= set(text.split())
-
 if arg.verbose:
   print(f'query set: {query}')
 
-# CONNECT TO Neo4J
-neo = Graph("bolt://localhost:7687", auth=("neo4j", "a"))
-nodes = NodeMatcher(neo)
-
-# COMPUTE POPULATION SIZE
+# Population size recovery
 population_size = nodes.match('Gene', taxon_id=arg.species).count()
 if arg.verbose:
-	print("population_size: ", population_size)
+	print("Population_size: ", population_size)
 
-# RETRIEVE TARGET SETS FOR QUERY
-path = '[:is_a|part_of|annotates*]' if arg.sets=='GOTerm' else ''
-cypher = f"MATCH (t:{arg.sets})-{path}->(n:Gene {{taxon_id:{arg.species} }}) WHERE n.id IN ['"+"', '".join(query)+"'] RETURN COUNT(DISTINCT t)"
+# Retrieve target sets for query
+path = '[:is_a|part_of|annotates*]' if arg.type=='GOTerm' else ''
+cypher = f"MATCH (t:{arg.type})-{path}->(n:Gene {{taxon_id:{arg.species} }}) WHERE n.id IN ['"+"', '".join(query)+"'] RETURN COUNT(DISTINCT t)"
 sample_size = neo.run(cypher).to_table()[0][0] # Get sample size for feedback purpose
-cypher = f"MATCH (t:{arg.sets})-{path}->(n:Gene {{taxon_id:{arg.species} }}) WHERE n.id IN ['"+"', '".join(query)+"'] RETURN DISTINCT t"
+cypher = f"MATCH (t:{arg.type})-{path}->(n:Gene {{taxon_id:{arg.species} }}) WHERE n.id IN ['"+"', '".join(query)+"'] RETURN DISTINCT t"
 sets = neo.run(cypher)
 if arg.verbose:
 	print(cypher)
-# EVALUATE SETS
-cpt = 0
-results = []
-for s in sets: #_ids:
-	cpt += 1
-	print(f"In progress : {cpt}/{sample_size} ({round(cpt/sample_siz*100,2}%)", end = "\r") # Feedback
+
+# Results recovery
+if arg.eval: # Stores the result from all methods for metrics evaluation
+	results = {'Binomial':[],'Coverage':[],'Hypergeometric':[],'Chi2':[]}
+else:
+	results = []
+
+# Metrics
+def binomial(c, q, t, g):
+	return binom.cdf(q - c, q, 1 - float(t) / g)
+
+def coverage(c, q, t):
+	return (c/q) * (c/t)
+
+def hypergeometric(q, t, g):
+	return hypergeom.cdf(q - t, g, int((1 - float(t)/g)*g), q)
+
+def chi2(c, q, t, g):
+	return chi2_contingency(np.array([[c, q-c],[t-c, g-q-t+c]]))[1]
+
+# Enrichment evaluation
+for count, s in enumerate(sets):
+	print(f"In progress : {count}/{sample_size} ({round(count/sample_size*100,1)}%)", end = "\r") # Feedback
 	sid = s['t']['id']
-	# RETRIEVE TARGET SET ELEMENTS
-	path = '[:is_a|part_of|annotates*1..20]' if arg.sets=='GOTerm' else ''
-	cypher = f"MATCH (t:{arg.sets})-{path}->(n:Gene {{taxon_id:{arg.species} }}) WHERE t.id='{sid}' RETURN n.id"
+	path = '[:is_a|part_of|annotates*1..20]' if arg.type=='GOTerm' else '' # Get correct path
+	cypher = f"MATCH (t:{arg.type})-{path}->(n:Gene {{taxon_id:{arg.species} }}) WHERE t.id='{sid}' RETURN n.id"
 	if arg.verbose:
 		print(cypher)
 	table = neo.run(cypher).to_table()
 	elements = set( map(lambda x: x[0], table))
-	common_elements = elements.intersection( query ) # C
+	common_elements = elements.intersection(query)
 	c = len(common_elements)
 	q = len(query)
 	t = len(elements)
 	g = population_size
 	if c < 2:
 		next
-	if arg.measure=='binomial': # binom.cdf(>=success, attempts, proba)
-		measure = binom.cdf(q - c, q, 1 - float(t) / g)
-	elif arg.measure == 'coverage':
-		measure = (c/q) * (c/t)
-	elif arg.measure =='hypergeometric':
-		measure = hypergeom.cdf(q - t, g, int((1 - float(t)/g)*g), q)
-	elif arg.measure =='chi2':
-		contengency_table = np.array([[c, q-c],[t-c, g-q-t+c]])
-		g, measure, dof, expctd = chi2_contingency(contengency_table)
-	elif arg.measure =='all':
-		break
-	else:
-		print(f'sorry, {arg.measure} not (yet) implemented')
-		exit(1)
-	r = { 'id': sid, 'desc': s['t']['desc'], 'common.n':c, 'target.n': t, 'measure': measure, 'elements.target': elements, 'elements.common': common_elements }
-	results.append( r )
+	if not arg.eval: # If we are not evaluating the metrics
+		if arg.measure=='binomial':
+			measure = binomial(c, q, t, g)
+		elif arg.measure == 'coverage':
+			measure = coverage(c, q, t)
+		elif arg.measure =='hypergeometric':
+			measure = hypergeometric(q, t, g)
+		elif arg.measure =='chi2':
+			measure = chi2(c, q, t, g)
+		else:
+			print(f'Sorry, {arg.measure} not implemented')
+			print("Please choose an implemented method : binomial, coverage, hypergeometric, chi2")
+			exit(1)
+		r = { 'id': sid, 'desc': s['t']['desc'], 'common.n':c, 'target.n': t, 'measure': measure, 'elements.target': elements, 'elements.common': common_elements }
+		results.append(r)
 
+	else: # Metrics evaluation
+		temp_results = [] # Stores results from all methods
+		temp_results.append(binomial(c, q, t, g)) # Binomial
+		temp_results.append(coverage(c, q, t)) # Coverage
+		temp_results.append(hypergeometric(q, t, g)) # Hypergeometric
+		temp_results.append(chi2(c, q, t, g)) # Chi2
+		for count, key in enumerate(results.keys()):
+			r = { 'id': sid, 'desc': s['t']['desc'], 'common.n':c, 'target.n': t, 'measure': temp_results[count], 'elements.target': elements, 'elements.common': common_elements }
+			results[key].append(r)
+	
 if arg.verbose:
-  print(results)
+	pprint.pprint(results, width=1)
 
-# PRINT SIGNIFICANT RESULTS
-i = 0
-results.sort(key=lambda an_item: an_item['measure'])
+# Print significant results
+print(f"Finished in {round(time.time() - start_time, 3)} seconds !   ")
 print("Significant results :")
-for r in results:
-	i += 1
-	# FDR
-	if arg.adjust and r['measure'] > arg.alpha * i / len(results): break
-	# limited output
-	if arg.limit > 0 and i>arg.limit: break
-	# alpha threshold
-	elif r['measure'] > arg.alpha: break
-	# OUTPUT
-	pval = "{:.4f}".format(r['measure']) if r['measure']>0.01 else "{:.2e}".format(r['measure'])
-	print(f"{r['id']}\t{pval}\t{r['common.n']}/{r['target.n']}\t{r['desc']}")
-	if arg.verbose : print(f"Common elements : {r['elements.common']}")
-		
-
+if not arg.eval:
+	results.sort(key=lambda an_item: an_item['measure'])
+	print("ID   p_value   ratio   Description   Common_elements") #################
+	for count, r in enumerate(results):
+		if arg.adjust and r['measure'] > arg.alpha * count+1 / len(results): break # FDR
+		if arg.limit > 0 and count+1>arg.limit: break # Limited output
+		elif r['measure'] > arg.alpha: break # Alpha threshold
+		# OUTPUT
+		pval = "{:.4f}".format(r['measure']) if r['measure']>0.01 else "{:.2e}".format(r['measure'])
+		print(f"{r['id']}\t{pval}\t{r['common.n']}/{r['target.n']}\t{r['desc']}", end=' ')
+		if arg.verbose : 
+			print(r['elements.common'])
+		else :
+			print(len(r['elements.common']))
+else:
+	for key in results.keys():
+		results[key].sort(key=lambda an_item: an_item['measure'])
+		print(f"\n{key} :")
+		print("ID   p_value   ratio   Description   Common_elements") #################
+		for count, r in enumerate(results[key]):
+			if arg.adjust and r['measure'] > arg.alpha * count+1 / len(results): break # FDR
+			if arg.limit > 0 and count+1>arg.limit: break # Limited output
+			elif r['measure'] > arg.alpha: break # Alpha threshold
+			# OUTPUT
+			pval = "{:.4f}".format(r['measure']) if r['measure']>0.01 else "{:.2e}".format(r['measure'])
+			print(f"{r['id']}\t{pval}\t{r['common.n']}/{r['target.n']}\t{r['desc']}", end=' ')
+			if arg.verbose : 
+				print(r['elements.common'])
+			else :
+				print(len(r['elements.common']))
