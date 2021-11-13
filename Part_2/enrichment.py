5076046dbcf1f5a78faa5ef16f5149f13ad92d3d
Victoria FATHI <victoria.fathi@gmail.com>
2021-11-13 12:36:54 +0100

    refactor enrichment.py

--- Part_2/enrichment.py
+++ Part_2/enrichment.py
@@ -11,11 +11,11 @@ import numpy as np
 # SCRIPT PARAMETERS
 parser = argparse.ArgumentParser(description='Search enriched terms/categories in the provided (gene) set')
 parser.add_argument('-q', '--query', required=True, help='Query set.')
-parser.add_argument('-t', '--sets', required=True, help='Target sets node type.')
+parser.add_argument('-t', '--target_type', required=True, help='Target sets node type [GOTerm, Interpro, Keyword, Pathway, PubMed, TU"].')
 parser.add_argument('-s', '--species', required=True, type=int, help='Taxon id.')
 parser.add_argument('-a', '--alpha', required=False, type=float, default=0.05, help='Significance threshold.')
 parser.add_argument('-c', '--adjust', required=False, action="store_true", help='Adjust for multiple testing (FDR).')
-parser.add_argument('-m', '--measure', required=False, default='binomial', help='Dissimilarity index: binomial (default), hypergeometric, chi2 or coverage. chi2 and coverage are NOT YET IMPLEMENTED')
+parser.add_argument('-m', '--measure', required=False, default='binomial', help='Dissimilarity index: binomial (default), hypergeometric, chi2 or coverage')
 parser.add_argument('-l', '--limit', required=False, type=int, default=0, help='Maximum number of results to report.')
 parser.add_argument('-v', '--verbose', required=False, action="store_true", help='Talk a lot.')
 param = parser.parse_args()
@@ -42,79 +42,74 @@ population_size = nodes.match('Gene', taxon_id=param.species).count()
 if param.verbose:
 	print("population_size: ", population_size)
 
-# RETRIEVE TARGET SETS FOR QUERYc
-path = '[:is_a|part_of|annotates*]' if param.sets=='GOTerm' else ''
-cypher = f"MATCH (t:{param.sets})-{path}->(n:Gene {{taxon_id:{param.species} }}) WHERE n.id IN ['"+"', '".join(query)+"'] RETURN DISTINCT t"
+# RETRIEVE TARGET SETS FOR QUERY
+path = '[:is_a|part_of|annotates*]' if param.target_type =='GOTerm' else ''
+cypher = f"MATCH (t:{param.target_type})-{path}->(n:Gene {{taxon_id:{param.species} }}) WHERE n.id IN ['"+"', '".join(query)+"'] RETURN DISTINCT t.id"
 if param.verbose:
 	print(cypher)
-sets = neo.run(cypher)
-#cypher = f"MATCH (t:{param.sets})-{path}->(n:Gene {{taxon_id:{param.species} }}) WHERE n.id IN ['"+"', '".join(query)+"'] RETURN count(DISTINCT t)"
-#aze = neo.run(cypher).to_table()
-#print(aze[0][0])
+target_sets = set(map(lambda x: x[0], neo.run(cypher).to_table()))
+target_sets_size = len(target_sets)
 
 # EVALUATE SETS
-i = 0
 results = []
 query_size = len(query)
 
-for s in sets: #_ids:
-	sid = s['t']['id']
+for i, target in enumerate(target_sets): #_ids:
+	# PROGRESSION STATUS
+	print(f"In progress : {i}/{target_sets_size} ({round(i/target_sets_size*100,1)}%)", end = "\r")
+
 	# RETRIEVE TARGET SET ELEMENTS
-	path = '[:is_a|part_of|annotates*1..20]' if param.sets=='GOTerm' else ''
-	cypher = "MATCH (t:{})-{}->(n:Gene {{taxon_id:{} }}) WHERE t.id='{}' RETURN n.id".format(param.sets, path, param.species, sid)
+	path = '[:is_a|part_of|annotates*1..20]' if param.target_type=='GOTerm' else ''
+	cypher = "MATCH (t:{})-{}->(n:Gene {{taxon_id:{} }}) WHERE t.id='{}' RETURN n.id".format(param.target_type, path, param.species, target)
+	
 	if param.verbose:
 		print(cypher)
+	
 	table = neo.run(cypher).to_table()
-	elements = set(map(lambda x: x[0], table))
-	i += 1
-	print(i)
-	common_elements = elements.intersection(query)
+	target_elements = set(map(lambda x: x[0], table))
+	common_elements = target_elements.intersection(query)
+
+	common_elements_size = len(common_elements)
+	target_elements_size = len(target_elements)
+
 	if len(common_elements) < 2:
 		next
 	if param.measure == 'coverage':
-		measure = (len(common_elements)/len(query)) * (len(common_elements)/len(elements))
+		measure = (common_elements_size/query_size * (common_elements_size/target_elements_size))
 	if param.measure =='binomial': # binom.cdf(>=success, attempts, proba)
-		# prendre le probleme à l'envers
-		# quelle est la probabilité qu'il y ait x elements pas en commun, 
-		# sachant un tirage de z elements et que la probabilité de tiré des elements 
-		# pas en commun est 1 - proba de tiré des elements en commun
-		measure = binom.cdf(query_size - len(common_elements), query_size, 1 - float(len(elements))/population_size)
+		measure = binom.cdf(query_size - len(common_elements), query_size, 1 - float(target_elements_size)/population_size)
 	if param.measure =='hypergeometric':
-	measure = hypergeom.cdf(query_size - len(common_elements),population_size, int((1 - float(len(elements))/population_size)*population_size), query_size)
+		measure = hypergeom.cdf(query_size - common_elements_size, population_size, int((1 - float(target_elements_size)/population_size)*population_size), query_size)
 	if param.measure =='chi2':
-		a = len(common_elements)
-		b = len(query) - len(common_elements)
-		c = len(elements) - len(common_elements)
-		d =  population_size - len(query) - len(elements) + len(common_elements)
+		unique_query = query_size - common_elements_size
+		unique_target = target_elements_size - common_elements_size
+		residue =  population_size - query_size - target_elements_size + common_elements_size
 		contengency_table = np.array([
-			[a, b],
-			[c, d]
+			[common_elements_size, unique_query],
+			[unique_target, residue]
 			])
-		z, measure, e, r = chi2_contingency(contengency_table)
-	#else:
-	#	print(f'sorry, {param.measure} not (yet) implemented')
-	#	exit(1)
-	r = { 'id': sid, 'desc': s['t']['desc'], 'common.n':len(common_elements), 'target.n': len(elements), 'measure': measure, 'elements.target': elements, 'elements.common': common_elements }
-	results.append( r )
+		measure = chi2_contingency(contengency_table)[1]
+	
+	# RESULTS
+	r = { 'id': target, 'common_size':len(common_elements), 'target_size': len(target_elements), 'measure': measure}
+	results.append(r)
+
 if param.verbose:
   print(results)
 
 # PRINT SIGNIFICANT RESULTS
 results.sort(key=lambda an_item: an_item['measure'])
-i=1
+
 for r in results:
 	# FDR
 	if param.adjust and r['measure'] > param.alpha * i / len(results): break
 	# limited output
 	
-	if param.limit > 0 and i>param.limit: break
+	if param.limit > 0 and i+1>param.limit: break
 	# alpha threshold
 	
 	elif r['measure'] > param.alpha: break
 	# OUTPUT
 	pval = "{:.4f}".format(r['measure']) if r['measure']>0.01 else "{:.2e}".format(r['measure'])
-	
-	print("{}\t{}\t{}/{}\t{}\t{}".format( r['id'], pval, r['common.n'], r['target.n'], r['desc'], ', '.join(r['elements.common'])))
-	i+=1
-
+	print(f"{r['id']}\t{pval}\t{r['common_size']}\t{r['target_size']}")
 
