76aff29713b0c281110c59657c8df22597137ac5
Victoria FATHI <victoria.fathi@gmail.com>
2021-11-15 18:41:35 +0100

    add final results

--- Part_2/README.md
+++ Part_2/README.md
@@ -1,31 +1,23 @@
 # Welcome to Enrichment analysis project
 
-This Gitlab summarizes the work done during IDH (Heterogenous Data Integration) class as part of the Master Sc in Bioinformatics of Toulouse.
+In this GitLab project, you will find all files and documentation related to E.coli enrichment analysis project.
 
-**Authors :**
-- **Victoria Fathi**
-- **Lou Duron**
+:warning: You are on the branch "Vic", you will find here the documentation regarding the work done by Victoria FATHI :warning: 
 
-## Organisation
+## Organization
 
-- **benchmark_data** : all data related to the benchmark
-- **raw_data** : all data related to E.coli retrieved form UniProt and EcoCyc 
-- **import** : all files needed to create the data base
-- **query_sets** : query sets for enrichment analysis
-- **Neo4j_Roland.py** : initial script (this has been modified in each branch)
+- **benchmark_data/** : all data related to the benchmark
+- **benchmark_results/**: results from bencharmking analysis
+- **raw_data**/ : all data related to E.coli retrieved form UniProt and EcoCyc 
+- **import/**: all files needed to create the data base
+- **query_sets/**: query sets for enrichment analysis
+- **results_set20/**: results from set20 analysis
 - **data_integration_procedure.hmtl** : procedure used to integrate E.coli data in Neo4j database.
 - **get_data.Rmd** : procedure used to prepare data for integration
 - **get_benchmark_data.r** : procedure used to prepare benchmark data for analysis
 - **benchmark_analysis.Rmd** : procedure used for benchmark analysis
-
-
-## Description
-
-This project is divided in two parts.
-- Data intregration
-- Enrichment analysis
-
-The first part has been done in collaboration as part of IDH class. The second part had to be done individualy, therefore this GitLab project has been divided in two branches (lou and vic).
+- **set.M2.20.txt**:: unknown set to analyze
+- **idh_env.yml** : to recreate conda environment
 
 ## How to navigate between branches
 
@@ -54,4 +46,56 @@ git clone git@gitlab.com:best-partnership/m2idh.git
 cd m2idh/Part_2
 git checkout vic
 ```
+## Environment 
+Note that we used conda 4.10.1 during this project and **idh_env.yml** is available on the gitlab project to recreate the environment.
 
+## Documentation
+
+# Database creation - get_data.Rmd documentation
+
+Required packages:
+- Tidyverse 1.3.1
+
+To reproduce data parsing you will need to change the working directory: setwd('/<path to Part2>/Part_2/')
+
+
+# Enrichement analysis - Enrichment.py documentation
+
+Required packages:
+- numpy 1.19.5
+- scipy 1.5.3 
+- py2neo 2021.2.3 
+- pandas 1.1.5
+
+
+## How to use
+```tiddlywiki
+enrichment.py -p [PASSWORD] -q [QUERY] -t [NODE_TYPE] -m [METRICS] [OPTION]
+                     
+-p, --password [required] password neo4j database
+-q, --query [required] path to query set
+-t --target_type [required] Target sets node type [GOTerm, Interpro, Keyword,Pathway, PubMed, TU"]
+-m --measure [required] Dissimilarity index: binomial (default), hypergeometric, chi2 or coverage
+-s --species taxon id (default=511145)
+-a --alpha [optional] Significance threshold (default = 0.05)
+-c, --adjust [optional] Adjust for multiple testing (FDR)
+-l --limit [optional] Maximum number of results to report.
+-w, --write [optional] path and name for results (tsv file)
+-v, --verbose [optional] print intermediary results/queries
+```
+
+## Example of use
+
+```typescript
+./enrichment.py -p password -q query_sets/set01.txt -t GOTerm -s 511145 alpha 0.2 -c -m chi2 -l 10
+```
+This will print the first 10 enriched GOTerm in set01.txt, using Chi-Square Test of Independence , with a significance threshold of 0.2 and using adjust for mutliple testing
+
+```typescript
+./enrichissement.py -p password -q query_sets/set01.txt -t Keyword -s 511145 -e -l 5
+```
+This will print the first 5 enriched Keyword in set01.txt with the 4 methods.
+
+```
+./enrichment.py -p pwd -q benchmark_data/big_set.txt -t GOTerm -m binomial -c -w benchmark_results/big_GOTerm_binomial.tsv
+```
--- Part_2/enrichment.py
+++ Part_2/enrichment.py
@@ -3,22 +3,28 @@
 import argparse
 from os.path import isfile
 from scipy.stats import binom, hypergeom, chi2_contingency
-from pprint import pprint
 from py2neo import *
 from py2neo.matching import *
 import numpy as np
+import pandas as pd
+from termcolor import colored
+
 
 # SCRIPT PARAMETERS
 parser = argparse.ArgumentParser(description='Search enriched terms/categories in the provided (gene) set')
-parser.add_argument('-q', '--query', required=True, help='Query set.')
-parser.add_argument('-t', '--target_type', required=True, help='Target sets node type [GOTerm, Interpro, Keyword, Pathway, PubMed, TU"].')
-parser.add_argument('-s', '--species', required=True, type=int, help='Taxon id.')
-parser.add_argument('-a', '--alpha', required=False, type=float, default=0.05, help='Significance threshold.')
-parser.add_argument('-c', '--adjust', required=False, action="store_true", help='Adjust for multiple testing (FDR).')
-parser.add_argument('-m', '--measure', required=False, default='binomial', help='Dissimilarity index: binomial (default), hypergeometric, chi2 or coverage')
+parser.add_argument('-p', '--password', required=True, help='password neo4j database')
+parser.add_argument('-q', '--query', required=True, help='path to query set')
+parser.add_argument('-t', '--target_type', required=True, choices=["GOTerm", "Interpro", "Keyword", "Pathway", "PubMed", "TU"], help='Target sets node type [GOTerm, Interpro, Keyword, Pathway, PubMed, TU"]')
+parser.add_argument('-s', '--species', required=False, type=int, help='Taxon id', default=511145)
+parser.add_argument('-a', '--alpha', required=False, type=float, default=0.05, help='Significance threshold')
+parser.add_argument('-c', '--adjust', required=False, action="store_true", help='Adjust for multiple testing (FDR)')
+parser.add_argument('-m', '--measure', required=False, choices = ["binomial", "hypergeometric", "coverage", "chi2"], default='binomial', help='Dissimilarity index: binomial (default), hypergeometric, chi2 or coverage')
 parser.add_argument('-l', '--limit', required=False, type=int, default=0, help='Maximum number of results to report.')
-parser.add_argument('-v', '--verbose', required=False, action="store_true", help='Talk a lot.')
+parser.add_argument('-r', '--revigo', required=False, action="store_true", help='only if --write, write id and pvalue for revigo')
+parser.add_argument('-v', '--verbose', required=False, action="store_true", help='print intermediary results/queries')
+parser.add_argument('-w', '--write', required=False, help='path and name for results (tsv file)')
 param = parser.parse_args()
+
 
 # LOAD QUERY
 text = param.query
@@ -34,7 +40,7 @@ if param.verbose:
   print(f'query set: {query}')
 
 # CONNECT TO Neo4J
-neo = Graph("bolt://localhost:7687", auth=("neo4j", "a"))
+neo = Graph("bolt://localhost:7687", auth=("neo4j", param.password))
 nodes = NodeMatcher(neo)
 
 # COMPUTE POPULATION SIZE
@@ -44,12 +50,16 @@ if param.verbose:
 
 # RETRIEVE TARGET SETS FOR QUERY
 path = '[:is_a|part_of|annotates*]' if param.target_type =='GOTerm' else ''
-cypher = f"MATCH (t:{param.target_type})-{path}->(n:Gene {{taxon_id:{param.species} }}) WHERE n.id IN ['"+"', '".join(query)+"'] RETURN DISTINCT t.id"
+cypher = f"MATCH (t:{param.target_type})-{path}->(n:Gene {{taxon_id:{param.species} }}) WHERE n.id IN ['"+"', '".join(query)+"'] RETURN DISTINCT t"
 if param.verbose:
 	print(cypher)
-target_sets = set(map(lambda x: x[0], neo.run(cypher).to_table()))
-target_sets_size = len(target_sets)
 
+target_sets = neo.run(cypher).to_table()
+target_sets_size = len(target_sets)
+if target_sets_size == 0:
+	print(f"{cypher} returned an empty results.\nCheck if genes are in the database or in the correct format (bnumber)")
+	exit(1)
+	
 # EVALUATE SETS
 results = []
 query_size = len(query)
@@ -59,8 +69,9 @@ for i, target in enumerate(target_sets): #_ids:
 	print(f"In progress : {i}/{target_sets_size} ({round(i/target_sets_size*100,1)}%)", end = "\r")
 
 	# RETRIEVE TARGET SET ELEMENTS
+	target_id = target[0]['id']
 	path = '[:is_a|part_of|annotates*1..20]' if param.target_type=='GOTerm' else ''
-	cypher = "MATCH (t:{})-{}->(n:Gene {{taxon_id:{} }}) WHERE t.id='{}' RETURN n.id".format(param.target_type, path, param.species, target)
+	cypher = "MATCH (t:{})-{}->(n:Gene {{taxon_id:{} }}) WHERE t.id='{}' RETURN n.id".format(param.target_type, path, param.species, target_id)
 	
 	if param.verbose:
 		print(cypher)
@@ -72,16 +83,21 @@ for i, target in enumerate(target_sets): #_ids:
 	common_elements_size = len(common_elements)
 	target_elements_size = len(target_elements)
 
+	# ENRICHMENT MEASURES
 	if len(common_elements) < 2:
 		next
 	if param.measure == 'coverage':
+		# based of shared elements
 		measure = (common_elements_size/query_size * (common_elements_size/target_elements_size))
+
 	if param.measure =='binomial': # binom.cdf(>=success, attempts, proba)
 		measure = binom.cdf(query_size - len(common_elements), query_size, 1 - float(target_elements_size)/population_size)
-	if param.measure =='hypergeometric':
+
+	if param.measure =='hypergeometric': #hypergeometric.cdf(>=sucess, total number of object ,proba, attempts)
 		inverse_freq = int((1 - float(target_elements_size)/population_size)*population_size)
 		measure = hypergeom.cdf(query_size - common_elements_size, population_size, inverse_freq, query_size)
-	if param.measure =='chi2':
+
+	if param.measure =='chi2': 
 		unique_query = query_size - common_elements_size
 		unique_target = target_elements_size - common_elements_size
 		residue =  population_size - query_size - target_elements_size + common_elements_size
@@ -92,7 +108,7 @@ for i, target in enumerate(target_sets): #_ids:
 		measure = chi2_contingency(contengency_table)[1]
 	
 	# RESULTS
-	r = { 'id': target, 'common_size':len(common_elements), 'target_size': len(target_elements), 'measure': measure}
+	r = { 'id': target_id, 'desc': target[0]["desc"], 'common_size':len(common_elements), 'target_size': len(target_elements), 'measure': measure}
 	results.append(r)
 
 if param.verbose:
@@ -100,18 +116,27 @@ if param.verbose:
 
 # PRINT SIGNIFICANT RESULTS
 results.sort(key=lambda an_item: an_item['measure'])
+adjusted_results = []
+for r in results:
+	# FDR
+	if param.adjust and r['measure'] > param.alpha * i / len(results) and param.measure != 'coverage': break
+	
+	# limited output
+	if param.limit > 0 and i+1>param.limit: break
+	# alpha threshold
+	
+	elif r['measure'] > param.alpha: break
+	# OUTPUT
+	pval = "{:.4f}".format(r['measure']) if r['measure']>0.01 else "{:.2e}".format(r['measure'])
+	adjusted_results.append({"id": r['id'], "desc": r['desc'], "pval":pval, "common_size":r['common_size'], "target_size":r['target_size']})
 
-if param.measure != "coverage":
-	for r in results:
-		# FDR
-		if param.adjust and r['measure'] > param.alpha * i / len(results): break
-		
-		# limited output
-		if param.limit > 0 and i+1>param.limit: break
-		# alpha threshold
-		
-		elif r['measure'] > param.alpha: break
-		# OUTPUT
-		pval = "{:.4f}".format(r['measure']) if r['measure']>0.01 else "{:.2e}".format(r['measure'])
-		print(f"{r['id']}\t{pval}\t{r['common_size']}\t{r['target_size']}")
+print("")
+df_adjusted_results = pd.DataFrame(adjusted_results)	
+print(df_adjusted_results)
 
+if param.write:
+	if param.revigo:
+			df_adjusted_results[['id', 'pval']].to_csv(param.write, sep = "\t", index= False)
+	else:
+		df_adjusted_results.to_csv(param.write, sep = "\t", index= False)
+	print(colored(f"file written: {param.write}", 'green')) 
